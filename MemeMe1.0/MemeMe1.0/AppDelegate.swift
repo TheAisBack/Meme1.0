2943c3a6dd7b802a2343c48f589ae27892caa314
Theaisback <alanhekle@hotmail.com>
2017-07-10 01:09:33 -0500

    adding

--- MemeMe1.0/MemeMe1.0/AppDelegate.swift
+++ MemeMe1.0/MemeMe1.0/AppDelegate.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  AppDelegate.swift
 //  MemeMe1.0
 //
--- MemeMe1.0/MemeMe1.0/ViewController.swift
+++ MemeMe1.0/MemeMe1.0/ViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imagePickerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        self.present(pickerController, animated: true, completion: nil)
     }
-
-
 }
-
